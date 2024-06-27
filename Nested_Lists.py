789bb433e2c431593ed0d88e1ed4f37d3d0f2e80
Mahdi Haroun <mhd0228222@ju.edu.jo>
2024-06-27 19:59:44 +0300

    NESTED LISTS

---
+++ Nested_Lists.py
@@ -0,0 +1,22 @@
+#return hourse
+animals = [['cat', 'dog', 'mouse'], ['horse', 'cow', 'goat'], ['cheetah', 'giraffe', 'rhino']]
+idx1 = animals [1] [0] 
+print (idx1)  
+#****************************************************
+nested1 = [['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
+y = nested1[1]
+print(y)
+print(y[0])
+
+print([10, 20, 30][1])
+print(nested1[1][0]) 
+#****************************************************
+
+nested1 = [['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
+print(nested1[0])
+print(len(nested1))
+nested1.append(['i'])
+print("-------")
+for L in nested1:
+    print(L)
+#****************************************************
