df9aca44fdfce5e02c0e4b22080d09bb64020caa
Mahdi Haroun <mhd0228222@ju.edu.jo>
2024-07-01 20:40:26 +0300

    filter end

---
+++ Filter.py
@@ -0,0 +1,16 @@
+#turning this to new by filter method : 
+def keep_evens(nums):
+    new_list = []
+    for num in nums:
+        if num % 2 == 0:
+            new_list.append(num)
+    return new_list
+
+print(keep_evens([3, 4, 6, 7, 0, 1]))
+ 
+#****************************************************
+def keep_evens(nums):
+    new_seq = filter(lambda num: num % 2 == 0, nums)
+    return list(new_seq)
+
+print(keep_evens([3, 4, 6, 7, 0, 1]))  
